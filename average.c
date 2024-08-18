ecb2911ea6fa8eac65c03d06f6c900dd84e41a5e
sarwan <sarwanazhar12345@gmail.com>
2024-08-18 13:07:29 +0500

    average

---
+++ average.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+float average(int, int, int);
+
+float average(int a, int b, int c) {
+    return (a+b+c)/3.0;
+}
+
+int main () {
+    float averaged = average(3,50,20);
+
+    printf("the average of 3,50,20 is %.2f\n", averaged);
+    return 0;
+}
